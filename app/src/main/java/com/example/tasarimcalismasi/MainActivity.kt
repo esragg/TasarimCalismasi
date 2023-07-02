22eb07d7444acd1702a1043735faeb033ff53112
Esra GARCIA GORMAN <esragarcia@hotmail.com>
2023-07-02 14:19:26 +0200

    Jetpack Compose TasarimCalismasi alignment (ters eksende) hizalama

--- app/src/main/java/com/example/tasarimcalismasi/MainActivity.kt
+++ app/src/main/java/com/example/tasarimcalismasi/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -42,8 +43,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Sayfa() {
 
-    Row(horizontalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxWidth()){
+    Column(horizontalAlignment = Alignment.CenterHorizontally){
         Box(modifier = Modifier
             .size(80.dp)
             .background(Color.Red))

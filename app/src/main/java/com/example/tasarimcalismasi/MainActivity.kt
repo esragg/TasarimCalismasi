1c2b2a28ac39e5852cdb3a185c90f20478046698
Esra GARCIA GORMAN <esragarcia@hotmail.com>
2023-07-02 19:08:42 +0200

    Jetpack Compose TasarimCalismasi Widget ozellestirme

--- app/src/main/java/com/example/tasarimcalismasi/MainActivity.kt
+++ app/src/main/java/com/example/tasarimcalismasi/MainActivity.kt
@@ -1,26 +1,28 @@
 package com.example.tasarimcalismasi
 
-import androidx.compose.ui.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tasarimcalismasi.ui.theme.TasarimCalismasiTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,19 +44,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Sayfa() {
-
-    Column(horizontalAlignment = Alignment.CenterHorizontally){
-        Box(modifier = Modifier
-            .size(80.dp)
-            .background(Color.Red))
-        Box(modifier = Modifier
-            .size(50.dp)
-            .background(Color.Green))
-        Box(modifier = Modifier
-            .size(100.dp)
-            .background(Color.Blue))
+    Column {
+        KirmiziKare()
+        YesilKare()
+        Yazi("Merhaba",50)
     }
+}
 
+@Composable
+fun KirmiziKare() {
+    Box(modifier = Modifier
+        .size(100.dp)
+        .background(Color.Red))
+}
+@Composable
+fun YesilKare() {
+    Box(modifier = Modifier
+        .size(100.dp)
+        .background(Color.Green))
+}
+@Composable
+fun Yazi(icerik:String, yaziBoyutu:Int) {
+    Text(text = icerik, fontSize = yaziBoyutu.sp)
 }
 
 @Preview(showBackground = true)

5b521bb06d47922127eb7b7fd409642bac58d97d
Esra GARCIA GORMAN <esragarcia@hotmail.com>
2023-07-03 12:52:08 +0200

    Jetpack Compose coklu dil destegi

--- app/src/main/java/com/example/tasarimcalismasi/MainActivity.kt
+++ app/src/main/java/com/example/tasarimcalismasi/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -20,6 +22,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -44,10 +47,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Sayfa() {
-    Column {
-        KirmiziKare()
-        YesilKare()
-        Yazi("Merhaba",50)
+    Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = stringResource(id = R.string.textYazi), fontSize = 50.sp)
+        Button(onClick = { }) {
+            Text(text = stringResource(id = R.string.buttonYazi), fontSize = 24.sp)
+        }
     }
 }
 
@@ -68,7 +74,7 @@ fun Yazi(icerik:String, yaziBoyutu:Int) {
     Text(text = icerik, fontSize = yaziBoyutu.sp)
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, locale = "tr")
 @Composable
 fun GreetingPreview() {
     TasarimCalismasiTheme {
